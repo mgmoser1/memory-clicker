9b20523f4aad51327795d358dfdc22e62e5d2ce6
MGM <mgmoser1@users.noreply.github.com>
2019-07-20 17:07:28 -0500

    Updates

---
+++ precache-manifest.c1a3a606c7a9ab10891b924972be7e66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa64db90a871b01a27844496805a8f0d",
+    "url": "/memory-clicker/index.html"
+  },
+  {
+    "revision": "1fa6135e18f67ebbc9b3",
+    "url": "/memory-clicker/static/css/main.ccebcdca.chunk.css"
+  },
+  {
+    "revision": "1c21bba1fa135f4c879e",
+    "url": "/memory-clicker/static/js/2.b60b1bf2.chunk.js"
+  },
+  {
+    "revision": "1fa6135e18f67ebbc9b3",
+    "url": "/memory-clicker/static/js/main.030f8ce3.chunk.js"
+  },
+  {
+    "revision": "5dda29ad6b4129093183",
+    "url": "/memory-clicker/static/js/runtime~main.454febae.js"
+  }
+]);
